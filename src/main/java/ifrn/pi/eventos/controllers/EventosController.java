7d9e1693d81011d8a775bc88440e7a2b89b2c581
Júlia <julia.guimaraes@escolar.ifrn.edu.br>
2022-10-14 01:12:16 -0300

    Enviando dados do formulário para a aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,7 +1,10 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
@@ -10,5 +13,22 @@ public class EventosController {
 	public String form() {
 		return "formEvento";
 	}
-	
+
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		
+		System.out.println(evento);
+
+		return "evento-adicionado";
+	}
+
+	// @RequestMapping("/eventos/salvar")
+	// public String salvar(Evento preencher) {
+	// System.out.println("Nome: " + preencher.getNome());
+	// System.out.println("Local: " + preencher.getLocal());
+	// System.out.println("Data: " + preencher.getData());
+	// System.out.println("Horario: " + preencher.getHorario());
+	// return "salvar";
+	// }
+
 }
